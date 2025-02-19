668162944bce8f5d0a4fe3679e55ff32fbcb1809
Marcelino <marcelino85albuquerque@gmail.com>
2025-02-19 17:26:42 -0300

    feat(main): Iniciando o Front-End com Next e implementando a tela de Home, Login e registro.

---
+++ src/app/login/page.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+import Link from "next/link" 
+
+export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+       await axios.post("http://localhost:3006/auth/login", { email, password });
+      alert("Login bem-sucedido!");
+      router.push("/dashboard");
+    } catch (error) {
+      alert({error, mensage:"Erro ao fazer login. Verifique suas credenciais"});
+    }
+  };
+
+  return (
+    <div className="bg-white p-6 rounded-lg shadow-md w-96">
+      <h2 className="text-2xl font-bold mb-4">Login</h2>
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="E-mail"
+          className="w-full p-2 border rounded mb-2"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Senha"
+          className="w-full p-2 border rounded mb-2"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button className="bg-blue-500 text-white px-4 py-2 rounded w-full">Entrar</button>
+        
+      <Link href="/register">
+        <p className="px-4 py-2 rounded-md">Registrar</p>
+      </Link>
+      <Link href="/">
+        <p className="px-4 py-2 rounded-md">Page</p>
+      </Link>
+        
+      </form>
+    </div>
+  );
+}
---
+++ src/app/register/page.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { useState } from "react";
+import axios from "axios";
+import Link from "next/link" 
+
+export default function RegisterPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleRegister = async (e: React.FormEvent) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      alert("As senhas não coincidem!");
+      return;
+    }
+    try {
+      await axios.post("http://localhost:3006/auth/register", { email, password });
+      alert("Usuário registrado com sucesso!");
+    } catch (error) {
+      alert({error, mensage:"Erro ao registrar usuário."});
+    }
+  };
+
+  return (
+    <div className="bg-white p-6 rounded-lg shadow-md w-96">
+      <h2 className="text-2xl font-bold mb-4">Registro</h2>
+      <form onSubmit={handleRegister}>
+        <input
+          type="email"
+          placeholder="E-mail"
+          className="w-full p-2 border rounded mb-2"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Senha"
+          className="w-full p-2 border rounded mb-2"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Confirme sua senha"
+          className="w-full p-2 border rounded mb-2"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+        <button className="bg-green-500 text-white px-4 py-2 rounded w-full">Registrar</button>
+
+      <Link href="/login">
+        <p className="px-4 py-2 rounded-md">Login</p>        
+      </Link>
+      <Link href="/">
+        <p className="px-4 py-2 rounded-md">Page</p>
+      </Link>
+      </form>
+    </div>
+  );
+}
